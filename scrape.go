0735b8bb8a3c3679444fc89ae93c254478eef4ca
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-07-16 19:41:12 -0500

    [update]

--- scrape.go
+++ scrape.go
@@ -1,77 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"bytes"
+	"encoding/json"
+	"net/http"
 
+	"github.com/droxey/getpunk/logger"
 	"github.com/gocolly/colly"
 )
+
+type Stock struct {
+	Name  string `json:"name"`
+	Price string `json:"price"`
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
 
-	scraper()
-	// Instantiate default collector
-	// c := colly.NewCollector()
+	// scraperHandler()
 
-	// // On every a element which has href attribute call callback
-	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-	// 	link := e.Attr("href")
+	host := "0.0.0.0:8888"
+	http.HandleFunc("/", scraperHandler)
+	logger.Log.Info("Server started: http://" + host)
 
-	// 	// Print link
-	// 	fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-	// })
+	err := http.ListenAndServe(host, nil)
+	if err != nil {
+		logger.Log.Error(err)
+		return
+	}
 
-	// // c.OnHTML("tr td:nth-of-type(2)", func(e *colly.HTMLElement) {
-	// // 	// fmt.Println("First column of a table row:", e.Text)
-	// // 	price := e.Attr("td:nth-of-type(3)")
-
-	// // 	fmt.Printf("Name and Price found: %q -> %s\n", e.Text, price)
-	// // })
-
-	// // Before making a request print "Visiting ..."
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Visiting", r.URL.String())
-	// })
-
-	// // Start scraping on https://finance.yahoo.com/cryptocurrencies
-	// c.Visit("https://finance.yahoo.com/cryptocurrencies")
 }
 
-func scraper() {
+func scraperHandler(w http.ResponseWriter, r *http.Request) { //w http.ResponseWriter, r *http.Request) {
 	c := colly.NewCollector()
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
+	c.OnHTML("body > div.main_content.project > tr td:nth-of-type(2) > tr td:nth-of-type(3) > b", func(e *colly.HTMLElement) {
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
-	})
+		stockPrice := new(Stock)
+		stockPrice.Name = "name"
+		stockPrice.Price = "price"
 
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Visited", r.Request.URL)
-	})
+		bf := bytes.NewBuffer([]byte{})
+		jsonEncoder := json.NewEncoder(bf)
+		jsonEncoder.SetEscapeHTML(false)
+		jsonEncoder.Encode(stockPrice)
 
-	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-	// 	e.Request.Visit(e.Attr("href"))
-	// })
-
-	c.OnHTML("tr td:nth-of-type(2)", func(e *colly.HTMLElement) {
-		fmt.Println("The name of the Cryptocurrency is:", e.Text)
-	})
-
-	c.OnHTML("tr td:nth-of-type(3)", func(e *colly.HTMLElement) {
-		fmt.Println("The price of the Cryptocurrency is:", e.Text)
-	})
-
-	c.OnXML("//h1", func(e *colly.XMLElement) {
-		fmt.Println(e.Text)
-	})
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(bf.Bytes())
 	})
 
 	c.Visit("https://finance.yahoo.com/cryptocurrencies")
